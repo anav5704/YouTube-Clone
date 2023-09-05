51b38bcb2596f3510431a40028d17a2dfafb6e7f
Anav Chand <anavkki@gmail.com>
2023-09-05 21:55:50 +1200

    :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () =>
   (
     <Stack zIndex={100} direction={"row"}  alignItems={"center"} p={2} sx={{borderBottom: "2px solid #262626",background: "#18181b", height: "10vh",  position: "sticky", top: 0, justifyContent: "space-between"}}>
       <Link to="/" style={{display: "flex", alignItems: "center"}}>
-        <img src="/fav.webp" alt="YouTube logo"  style={{height: "60px"}}/>
+        <img src="src/assets/fav.webp" alt="YouTube logo"  style={{height: "60px"}}/>
        </Link>
        <SearchBar />
     </Stack>
